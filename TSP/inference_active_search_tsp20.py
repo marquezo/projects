d0fcce9e68957e37a683aa776735e594e3232be0
Orlando M <orlando.m.bol@gmail.com>
2018-04-21 18:49:36 -0400

    Added cuda support

--- TSP/inference_active_search_tsp20.py
+++ TSP/inference_active_search_tsp20.py
@@ -30,6 +30,9 @@ tsp20 = CombinatorialRL(
 state = torch.load('Model/TSP20/tsp_model_TSP20_001.pt', map_location=lambda storage, loc:storage)
 tsp20.load_state_dict(state)
 
+if USE_CUDA:
+        tsp20 = tsp20.cuda()
+
 tsp20_dataset = pickle.load(open("test_data_set_20", "rb"))
 
 tsp20_loader = DataLoader(tsp20_dataset, batch_size=1, shuffle=False, num_workers=1)
@@ -38,6 +41,10 @@ tour_lengths = []
 
 for batch_id, sample_batch in enumerate(tsp20_loader):
         sample_batch = sample_batch.squeeze()
+
+        if USE_CUDA:
+                sample_batch = sample_batch.cuda()
+        #1e-5 was used by authors
         soln_sampling, min_tour_len = active_search(sample_batch, tsp20, 12800, 128, lr=1e-5)
         tour_lengths.append(min_tour_len)
         print ("batch id {}, result: {}".format(batch_id, min_tour_len))
