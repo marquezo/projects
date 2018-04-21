d6de2c57917241a73930228082c3566585468aac
Orlando M <orlando.m.bol@gmail.com>
2018-04-21 17:36:47 -0400

    Code to run the experiments

--- TSP/inference_tsp50.py
+++ TSP/inference_tsp50.py
@@ -1,5 +1,8 @@
 from CombinatorialRL import CombinatorialRL
+from torch.utils.data import DataLoader
+from inference import *
 import torch
+import pickle
 
 train_size = 1000000
 val_size = 10000
@@ -24,5 +27,19 @@ tsp50 = CombinatorialRL(
         attention="Bahdanau",
         use_cuda=USE_CUDA)
 
-state = torch.load('Model/TSP50/tsp_model_T50_001.pt')
+state = torch.load('Model/TSP50/tsp_model_T50_001.pt', map_location=lambda storage, loc:storage)
 tsp50.load_state_dict(state)
+
+tsp50_dataset = pickle.load(open("test_data_set_50", "rb"))
+
+tsp50_loader = DataLoader(tsp50_dataset, batch_size=1, shuffle=False, num_workers=1)
+
+sum_solutions = 0.0
+
+for batch_id, sample_batch in enumerate(tsp50_loader):
+        sample_batch = sample_batch.squeeze()
+        soln_sampling = sample_solution(sample_batch, tsp50, 128)
+        print reward_single_input(soln_sampling)
+        sum_solutions +=  soln_sampling
+
+print "Result: ", (sum_solutions/1000.0)
