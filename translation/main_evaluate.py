87b4394f1540f05c149cf94a8072ea81ae796b5d
Orlando Marquez <orlando.m.bol@gmail.com>
2018-10-18 23:02:43 -0400

    Added parameters to evaluator

--- translation/main_evaluate.py
+++ translation/main_evaluate.py
@@ -1,13 +1,27 @@
 from util import prepareData
 from models import EncoderRNN, DecoderRNN
 from train_utils import load_checkpoint, evaluateRandomly, get_datasets
-import torch
+import torch, argparse
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def main():
 
+def read_input():
+    parser = argparse.ArgumentParser(description="Evaluate an NMT Seq2Seq model")
+    parser.add_argument('checkpoint')
+    parser.add_argument('num_sentences', type=int)
+
+    args = parser.parse_args()
+
+    print("Read checkpoint: {}".format(args.checkpoint))
+    print("Read number of sentences: {}".format(args.num_sentences))
+
+    return args
+
+
+if __name__ == "__main__":
     from_lang, to_lang = 'eng', 'fra'
+    args = read_input()
 
     print("Using device {}".format(device))
     # To prepare the vocabularies, use all data
@@ -17,13 +31,11 @@ def main():
     _, dev_set = get_datasets(from_lang, to_lang)
 
     hidden_size = 256
-    encoder1 = EncoderRNN(input_lang.n_words, hidden_size).to(device)
-    decoder1 = DecoderRNN(output_lang.n_words, hidden_size, dropout_p=0.1).to(device)
+    encoder = EncoderRNN(input_lang.n_words, hidden_size, 1).to(device)
+    decoder = DecoderRNN(output_lang.n_words, hidden_size, 1, dropout_p=0.1).to(device)
 
-    encoder1, decoder1, _, _, _, _ = load_checkpoint("checkpoint.tar", encoder1, decoder1, None, None)
+    encoder, decoder, _, _, _ = load_checkpoint(args.checkpoint, encoder, decoder, None)
 
-    evaluateRandomly(dev_set, input_lang, output_lang, encoder1, decoder1, True, 3)
+    evaluateRandomly(dev_set, input_lang, output_lang, encoder, decoder, False, args.num_sentences)
 
     #TODO: Use BLEU score
-
-if __name__ == "__main__": main()
