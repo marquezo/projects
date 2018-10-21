21910b9adece39dbb212b1fdecaa17ebee171265
Orlando Marquez <orlando.m.bol@gmail.com>
2018-10-20 21:49:21 -0400

    Refactored attention layer a bit

--- translation/models.py
+++ translation/models.py
@@ -66,7 +66,6 @@ class DecoderRNN(nn.Module):
             #print("using attention in decoder:", output.size())
 
         output = F.relu(output) # [ batch_size x seq_len x hidden_size ]
-        print(output.size(), hidden.size())
         output, hidden = self.gru(output, hidden)
         output = self.out(output)
         output = self.softmax(output)
